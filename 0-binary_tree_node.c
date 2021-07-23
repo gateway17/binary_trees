dd01c18f3824b0e05071192fe9ce18e11a33b988
LuisColorado-tech <luiscolorado@outlook.com>
2021-07-22 20:24:50 -0500

    Add new feature and fix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
  * binary_tree_node - creates a binary tree node
@@ -7,24 +8,19 @@
  *
  * Return: pointer to the new node
  */
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *node;
 
     node = malloc(sizeof(binary_tree_t));
     if (node == NULL)
-    {
       return (NULL);
-    }
+      
     node->parent = parent;
     node->n = value;
+    node->left = new_node->right = NULL;
 
-    return node;
+    return (node);
     
 
 }
-
-
-
-
